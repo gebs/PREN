2d53f0416221d56870246c80c8b35d11443336f7
psychonoob <adrian.gebs@stud.hslu.ch>
2017-03-03 11:25:04 +0100

    -

--- src/ch/hslu/pren/team8/ziffer/Ziffererkennung.java
+++ src/ch/hslu/pren/team8/ziffer/Ziffererkennung.java
@@ -1,7 +1,22 @@
 package ch.hslu.pren.team8.ziffer;
 
+import org.opencv.core.Core;
+
 /**
  * Created by gebs on 3/3/17.
  */
 public class Ziffererkennung {
+
+    private final boolean useCamera = false;
+
+    public Ziffererkennung() {
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+    }
+
+    public void Start() {
+
+
+    }
+
+
 }
