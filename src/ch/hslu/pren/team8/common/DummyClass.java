06416267df62cc4906bb630a06f6aed57a7b8823
psychonoob <adrian.gebs@stud.hslu.ch>
2017-03-03 09:56:26 +0100

    DummyClasses for Packages

---
+++ src/ch/hslu/pren/team8/common/DummyClass.java
@@ -0,0 +1,7 @@
+package ch.hslu.pren.team8.common;
+
+/**
+ * Created by gebs on 3/3/17.
+ */
+public class DummyClass {
+}
---
+++ src/ch/hslu/pren/team8/ziffer/DummyClass.java
@@ -0,0 +1,7 @@
+package ch.hslu.pren.team8.ziffer;
+
+/**
+ * Created by gebs on 3/3/17.
+ */
+public class DummyClass {
+}
